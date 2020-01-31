23addae513a83a134647936ad38b3c361d9e76cd
mvonderosten <markvonderosten@gmail.com>
2020-01-31 00:03:38 +0000

    First Revision

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,40 @@
 /* eslint-disable */
 
 import "file-loader?name=[name].[ext]!../index.html";
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRanSuit());
+  document.querySelector(".card").innerHTML = generateRanNum();
+};
+
+let generateRanNum = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let rannum = Math.floor(Math.random() * numbers.length);
+
+  return numbers[rannum];
+};
+
+let generateRanSuit = () => {
+  let suit = ["heart", "spade", "diamond", "club"];
+
+  let ranSuit = Math.floor(Math.random() * suit.length);
+
+  return suit[ranSuit];
 };
